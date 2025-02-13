f045b7cd06d0765d13bd6c335619a831a03b4011
Angel Guevara <guevarangel14@gmail.com>
2025-02-13 01:01:13 -0400

    fix aliados form

--- src/aliados/components/AliadosModalForm/AliadosModalForm.tsx
+++ src/aliados/components/AliadosModalForm/AliadosModalForm.tsx
@@ -15,7 +15,7 @@ export const AliadosModalForm: React.FC<ModalProps> = ({onClose, editValues}) =>
             <div className='fixed bg-white p-6 rounded-md w-[90vw] h-[90vh] overflow-y-auto'>
 
                 <div className='flex flex-row justify-between mb-9'>
-                    <h2 className="text-xl font-medium self-center text-d-gray">{ true ? "Editar Aliado" : "Agregar Aliado"}</h2>
+                    <h2 className="text-xl font-medium self-center text-d-gray">{ editValues ? "Editar Aliado" : "Agregar Aliado"}</h2>
                     <svg onClick={onClose} className='self-center cursor-pointer' width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1 13L13 1M1 1L13 13" stroke="#9CA3AF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
